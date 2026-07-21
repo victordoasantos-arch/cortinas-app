5b68eacc0ed7e45be4b3ef4769239646187b1985
victordoasantos-arch <victordoasantos@gmail.com>
2026-07-20 22:06:35 -0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,3 @@
+// Dados do projeto Supabase (Project Settings -> API)
+export const SUPABASE_URL = 'https://fizosmjiwegepbizkjea.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpem9zbWppd2VnZXBiaXpramVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NzM4MzcsImV4cCI6MjEwMDE0OTgzN30.E5tx5xQEKr_vphVMsDjGn-hu56ODRe9SEbKSOsWrZ9k';
